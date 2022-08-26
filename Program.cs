e8f0e15ee2abf3c60431bf99a11abc3dcff8efd0
anand acchu <you@example.com>
2022-08-26 22:55:25 +0530

    Master

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace GenericDemo
+{
+    class Program
+    {
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------");
+        }
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine("---------------");
+        }
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------");
+        }
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+
+        }
+    }
