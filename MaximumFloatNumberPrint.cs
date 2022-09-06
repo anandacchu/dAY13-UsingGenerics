da840a71c963d8f70b8c61f20366df753eb4926a
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-06 13:11:53 +0530

    uc2

---
+++ MaximumFloatNumberPrint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class MaximumFloatNumberPrint<T> where T : IComparable
+    {
+        public static double MaximumFloatNumber(double num1, double num2, double num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return num1;
+        }
+    }
+}
