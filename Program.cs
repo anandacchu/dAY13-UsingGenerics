4fa6292dbd41fb6a3900d51e37b08d46d11c8155
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-06 12:50:31 +0530

    master

--- Program.cs
+++ Program.cs
@@ -33,6 +33,26 @@ namespace GenericDemo
             int[] intArray = { 1, 2, 3, 4, 5, 6, 7, };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
             char[] charArray = { 'a', 'b', 'c', 'd' };
+             //program.toprint<int>(intArray);
+            //program.toprint<double>(doubleArray);
+            //program.toprint<char>(charArray);
 
+            //print<int> obj1 = new print<int>(intArray);
+            //obj1.toprint();
+            //print<double> obj2 = new print<double>(doubleArray);
+            //obj2.toprint();
+            //print<char> obj3 = new print<char>(charArray);
+            //obj3.toprint();
+
+            //int a = 20, b = 30, c =80;
+
+            //print<int> obj1 = new print<int>();
+            //int val = obj1.PrintMaximum(a, b, c);
+
+            //Console.WriteLine(val);
+
+            int[] arr = { 112, 334, 456, 598, 623 };
+            ExtendTheMax<int> generic = new ExtendTheMax<int>(arr);
+            generic.PrintMaxValue();
         }
     }
