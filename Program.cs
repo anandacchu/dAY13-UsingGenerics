6f3ef620567e3b7242b33878d9d3ed1c7b4ebcd9
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-28 19:43:35 +0530

    maxstring

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace GenericsMaximumProblem
+{
+    class ProgramUC3
+    {
+        public static void Main()
+        {
+            MaximumStringUC3 max = new MaximumStringUC3();
+            max.GetInput();
+            max.Compare();
+        }
+    }
+}
