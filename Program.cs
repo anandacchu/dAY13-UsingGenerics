c3b6e484edde66ff5b39b8f2d2dbfdb3ab1d8fd0
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-28 19:37:45 +0530

    uc2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace GenericsMaximumProblem
+{
+    class ProgramUC2
+    {
+        public static void Main()
+        {
+            MaximumFloat max = new MaximumFloat();
+            max.GetInput();
+            max.Compare();
+        }
+    }
+}
