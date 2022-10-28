2980b026e7e0086560010259d657061d2e8e5630
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-28 19:34:09 +0530

    uc1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace GenericsMaximumProblem
+{
+    class ProgramUC1
+    {
+        public static void Main()
+        {
+            MaximumInt max = new MaximumInt();
+            max.GetInput();
+            max.Compare();
+        }
+    }
+}
